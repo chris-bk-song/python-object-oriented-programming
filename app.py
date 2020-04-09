6e9f02619cff5d7713831d0e24c0a86ca8a44138
Chris Song <chris.bk.song@gmail.com>
2020-04-09 09:57:17 -0400

    superhero exercise created by Chris Aquino

---
+++ app.py
@@ -0,0 +1,14 @@
+from lib.superhero import Superhero
+from lib.villain import Villain
+# from superhero import Superhero
+# from villain import Villain
+
+ww = Superhero('Wonder Woman', 'Diana Prince', 20, 100, 10)
+hulk = Superhero('Huuuulk', 'Dr Bruce Banner', 50, 500, 20)
+
+thanos = Villain('Thanos', 'Thanos', 30, 5000, 5)
+
+# Aspirational code follows. It will not work yet.
+# ww.strike(hulk)
+# thanos.strike(hulk)
+# hulk.strike(thanos)
